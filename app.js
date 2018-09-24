cef357bc5503a7530f3abf21325285bca1110d49
Clive Harber <clive@whitemonkeysoftware.co.uk>
2018-09-24 22:55:26 +0100

    Chapter 7 code

---
+++ app.js
@@ -0,0 +1,46 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+require('./app_api/models/db');
+
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const apiRoutes = require('./app_api/routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ app_server/controllers/locations.js
@@ -0,0 +1,188 @@
+const request = require('request');
+const apiOptions = {
+  server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://pure-temple-67771.herokuapp.com';
+}
+
+const formatDistance = (distance) => {
+  let thisDistance = 0;
+  let unit = 'm';
+  if (distance > 1000) {
+    thisDistance = parseFloat(distance / 1000).toFixed(1);
+    unit = 'km';
+  } else {
+    thisDistance = Math.floor(distance);
+  }
+  return thisDistance + unit;
+};
+
+const showError = (req, res, status) => {
+  let title = '';
+  let content = '';
+
+  if (status === 404) {
+    title = '404, page not found';
+    content = 'Oh dear, Looks like we can\'t find this page. Sorry';
+  } else {
+    title = `${status}, something's gone wrong`;
+    content = 'Something, somewhere, has gone just a little bit wrong.';
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title,
+    content
+  });
+};
+
+const renderHomepage = (req, res, responseBody) => {
+  let message = null;
+  if (!(responseBody instanceof Array)) {
+    message = 'API lookup error';
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = 'No places found nearby';
+    }
+  }
+  res.render('locations-list',
+    {
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+        title: 'Loc8r',
+        strapLine: 'Find places to work with wifi near you!'
+      },
+      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+      locations: responseBody,
+      message
+    }
+  );
+};
+
+const homelist = (req, res) => {
+  const path = '/api/locations';
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+    qs: {
+      lng: -0.7992599,
+      lat: 51.378091,
+      maxDistance: 20
+    }
+  };
+  request(
+    requestOptions,
+    (err, {statusCode}, body) => {
+      let data = [];
+      if (statusCode === 200 && body.length) {
+        data = body.map( (item) => {
+          item.distance = formatDistance(item.distance);
+          return item;
+        });
+      }
+      renderHomepage(req, res, data);
+    }
+  );
+};
+
+const renderDetailPage = (req, res, location) => {
+  res.render('location-info',
+    {
+      title: location.name,
+       pageHeader: {
+        title: location.name,
+      },
+      sidebar: {
+        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+        callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+      },
+      location
+    }
+  );
+};
+
+const getLocationInfo = (req, res, callback) => {
+  const path = `/api/locations/${req.params.locationid}`;
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {}
+  };
+  request(
+    requestOptions,
+    (err, {statusCode}, body) => {
+      const data = body;
+      if (statusCode === 200) {
+        data.coords = {
+          lng: body.coords[0],
+          lat: body.coords[1]
+        }
+        callback(req, res, data);
+      } else {
+        showError(req, res, statusCode);
+      }
+    }
+  );
+}
+
+const locationInfo = (req, res) => {
+  getLocationInfo(req, res,
+    (req, res, responseData) => renderDetailPage(req, res, responseData)
+  );
+}
+
+const renderReviewForm = (req, res, {name}) => {
+  res.render('location-review-form',
+    {
+      title: `Review ${name} on Loc8r` ,
+      pageHeader: { title: `Review ${name}` },
+      error: req.query.err
+    }
+  );
+};
+
+const addReview = (req, res) => {
+  getLocationInfo(req, res,
+    (req, res, responseData) => renderReviewForm(req, res, responseData)
+  );
+};
+
+const doAddReview = (req, res) => {
+  const locationid = req.params.locationid;
+  const path = `/api/locations/${locationid}/reviews`;
+  const postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'POST',
+    json: postdata
+  };
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect(`/location/${locationid}/review/new?err=val`);
+  } else {
+    request(
+      requestOptions,
+      (err, {statusCode}, {name}) => {
+        if (statusCode === 201) {
+          res.redirect(`/location/${locationid}`);
+        } else if (statusCode === 400 && name && name === 'ValidationError') {
+          res.redirect(`/location/${locationid}/review/new?err=val`);
+        } else {
+          showError(req, res, statusCode);
+        }
+      }
+    );
+  }
+};
+
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview,
+  doAddReview
+};
