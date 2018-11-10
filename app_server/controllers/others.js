d51b8846309171b784d212bca1b04d6345e2d6fd
Clive Harber <clive@whitemonkeysoftware.co.uk>
2018-11-10 21:01:11 +0000

    corrections

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,11 +1,9 @@
 /* GET homepage */
 const about = (req, res) => {
-  res.render('generic-text',
-    {
-      title: 'About Loc8r',
-      content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
-    }
-  );
+  res.render('generic-text', {
+    title: 'About Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
+  });
 };
 
 module.exports = {
