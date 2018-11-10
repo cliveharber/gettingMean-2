c0ab7ece0e28ed5273e6110e39eaa5980811438f
Clive Harber <clive@whitemonkeysoftware.co.uk>
2018-11-10 21:38:11 +0000

    corrections

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
