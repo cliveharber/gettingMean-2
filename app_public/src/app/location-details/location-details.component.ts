ceded1c371fe29872eec312627b30346ba2e8246
Clive Harber <clive@whitemonkeysoftware.co.uk>
2018-11-20 22:55:05 +0000

    change detection for reviews bug fix

--- app_public/src/app/location-details/location-details.component.ts
+++ app_public/src/app/location-details/location-details.component.ts
@@ -43,7 +43,9 @@ export class LocationDetailsComponent implements OnInit {
     if (this.formIsValid()) {
       this.loc8rDataService.addReviewByLocationId(this.location._id, this.newReview)
         .then((review: Review) => {
-          this.location.reviews.unshift(review);
+          let reviews = this.location.reviews.slice(0);
+          reviews.unshift(review);
+          this.location.reviews = reviews;
           this.resetAndHideReviewForm();
         });
     } else {
