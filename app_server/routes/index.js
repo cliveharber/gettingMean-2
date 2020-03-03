a9ff16de6dda08222c0d5c43af9ad19f1904136b
Amr <47042986+amroegyca@users.noreply.github.com>
2020-03-03 13:43:47 -0500

    First commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
+// const index = (req, res) => res.render('index', { title: 'Express '});
+// router.get('/', index);
 router.get('/', ctrlMain.index);
 
 module.exports = router;
